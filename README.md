043b8f4b9e649ce1951a38debcf9ae6b80dfa1fb
Steve Thompson <writetome51@gmail.com>
2019-02-02 14:26:54 -0700

    Added ability to parse dot-notation in the passed property argument.

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # getAverageFromProperty()
 ## getAverageFromProperty(property, objects): number
 
-Returns average value of property found in objects.  
+Returns average value of <b>property</b> found in <b>objects</b>.  
 Value of object property must be type 'number' and must be a finite number,  
-or else will error.
+or else will error.   
+<b>property</b> is a string that can include dot notation ( i.e,  'property.subproperty.subsubproperty' ) .
+
+Note:  <b>property</b> does not have to be an object key. It can also be an array index.  
+If you are getting the value of a nested array index, here you need to use dot-notation and not  
+square braces.  Example: `'1.0' instead of [1][0]`
 
 ## Installation
 
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-var getAverageFromProperty = require('./lib/index');
-
-module.exports = getAverageFromProperty;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var get_sum_average_product_1 = require("@writetome51/get-sum-average-product");
+var get_array_from_property_1 = require("@writetome51/get-array-from-property");
+// Returns average value of propertyWithNumberValue found in objects.
+// propertyWithNumberValue can contain dot-notation.
+function getAverageFromProperty(propertyWithNumberValue, objects) {
+    var numbers = get_array_from_property_1.getArrayFromProperty(propertyWithNumberValue, objects);
+    return get_sum_average_product_1.getAverage(numbers);
+}
+exports.getAverageFromProperty = getAverageFromProperty;
